9fe085a18b93e0e297becd32326335cbc52a600d
Aieda <aieda@arabella.ag>
2021-04-03 13:08:58 -0300

    estilizando a home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButoon';
+import styles from './home.module.scss';
 
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Início | Histórias pra brincar</title>
-    </Head>
-    <h1 >Hello world</h1>
+      <Head>
+        <title> Home| Histórias pra brincar</title>
+      </Head>
+      <main className={styles.contentContainer} >
+        <section className={styles.hero}>
+          <span> 👏 Olá, seja bem-vind@ ao</span>
+          <h1>Estórias pra <span>brincar</span></h1>
+          <p>
+          Tenha acesso a todas as publicações<br />
+          <span>por R$9,90 por mês</span>
+        </p>
+        <SubscribeButton />
+        </section>
+        <img src="images/avatar.svg" alt="Girl coding" />
+        
+      </main>
+     
     </>
   )
 }
