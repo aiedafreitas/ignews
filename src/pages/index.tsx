2757deb75a8bd54e35cf67a78d4235a63290064f
Aieda <aieda@arabella.ag>
2021-04-04 09:16:41 -0300

    conectando com a api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
-
+import { GetServerSideProps} from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButoon';
+import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
 
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
 
-export default function Home() {
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -16,7 +24,7 @@ export default function Home() {
           <h1>Estórias pra <span>brincar</span></h1>
           <p>
           Tenha acesso a todas as publicações<br />
-          <span>por R$9,90 aos mês</span>
+          <span>por {product.amount} aos mês</span>
         </p>
         <SubscribeButton />
         </section>
@@ -27,3 +35,20 @@ export default function Home() {
     </>
   )
 }
+ export const getServerSideProps: GetServerSideProps = async () => {
+   const price = await stripe.prices.retrieve ('price_1IcECEDUNPD7uz08VUZl0H34', {
+          expand: ['product']
+   })
+   const product = {
+     priceId: price.id,
+     amount: price.unit_amount / 100,
+   };
+
+
+   return {
+  props: {
+  product,
+}
+   }
+
+ }
