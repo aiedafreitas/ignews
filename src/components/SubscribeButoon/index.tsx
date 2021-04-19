1394870afda74c770e18c438d258e968465d2574
Aieda <aieda@arabella.ag>
2021-04-19 07:42:40 -0300

    stripe no subscribe

--- src/components/SubscribeButoon/index.tsx
+++ src/components/SubscribeButoon/index.tsx
@@ -1,10 +1,29 @@
+
+import { useSession, signIn } from 'next-auth/client';
 import styles from './styles.module.scss';
 
-export function SubscribeButton () {
+
+interface SubscribeButtonProps {
+    PriceId: string;
+}
+
+export function SubscribeButton ({PriceId}:SubscribeButtonProps) {
+    const [session] = useSession();
+   
+    function handleSubscribe() {
+        if (!session) {
+            signIn('github')
+            return;
+        }
+
+}
+
+
     return (
 <button
     type="button" 
-    className={styles.subscribeButton}>
+    className={styles.subscribeButton}
+    onClick={handleSubscribe}>
         Assine já
 
 </button>
