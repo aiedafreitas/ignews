427a5aeaf1828f549abf5e5a4cbb9b9826838209
Aieda <aieda@arabella.ag>
2021-06-11 14:10:34 -0300

    config login

--- src/components/SubscribeButoon/index.tsx
+++ src/components/SubscribeButoon/index.tsx
@@ -1,5 +1,7 @@
 
 import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 
@@ -10,12 +12,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton ({PriceId}:SubscribeButtonProps) {
     const [session] = useSession();
    
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if (!session) {
             signIn('github')
             return;
         }
 
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs()
+
+           await stripe.redirectToCheckout({sessionId})
+
+        } catch (err) {
+            alert(err.message);
+        }
 }
 
 
@@ -25,7 +39,6 @@ export function SubscribeButton ({PriceId}:SubscribeButtonProps) {
     className={styles.subscribeButton}
     onClick={handleSubscribe}>
         Assine já
-
 </button>
 
     );
